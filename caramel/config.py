fb3bf97ddae3cb81a0c499353853bb1567e7cec3
Amir Souri <ahsouri@gmail.com>
2023-02-08 12:30:29 -0500

    adding interpolator and updating other things

--- caramel/config.py
+++ caramel/config.py
@@ -5,16 +5,18 @@ import datetime
 @dataclass
 class satellite:
     vcd: np.ndarray
+    scd: np.ndarray
     time: datetime.datetime
     profile: np.ndarray
     tropopause: np.ndarray
+    latitude_center: np.ndarray
+    longitude_center: np.ndarray
     latitude_corner: np.ndarray
     longitude_corner: np.ndarray
     uncertainty: np.ndarray
     quality_flag: np.ndarray
     pressure_mid: np.ndarray
     averaging_kernels: np.ndarray
-    amf_total: np.ndarray
 
 @dataclass
 class ctm_model:
--- caramel/reader.py
+++ caramel/reader.py
@@ -5,11 +5,11 @@ import datetime
 import glob
 from joblib import Parallel, delayed
 from netCDF4 import Dataset
-from config import satellite
+from config import satellite, ctm_model
+from interpolator import interpolator
 import warnings
 
-
-warnings.filterwarnings("ignore",category=RuntimeWarning)
+warnings.filterwarnings("ignore", category = RuntimeWarning)
 
 def _read_nc(filename,var):
     # reading nc files without a group
@@ -31,25 +31,29 @@ def _read_group_nc(filename,num_groups,group,var):
     nc_fid.close()
     return np.squeeze(out)
 
-def tropomi_reader_no2_wrapper(fname) -> satellite:
+def tropomi_reader_no2_wrapper(fname:str,interpolation_flag = True) -> satellite:
     # a wrapper to run in parallel
     # read time
     time = np.array(_read_group_nc(fname,1,'PRODUCT','time')) +\
            np.nanmean(np.array(_read_group_nc(fname,1,'PRODUCT','delta_time')), axis=0)/1000
     time = np.squeeze(time)
-    satellite.time = datetime.datetime(2010, 1, 1) + datetime.timedelta(seconds=int(time))
+    tropomi_no2 = satellite
+    tropomi_no2.time = datetime.datetime(2010, 1, 1) + datetime.timedelta(seconds=int(time))
     # read lat/lon at corners
-    satellite.latitude_corner = _read_group_nc(fname,3,['PRODUCT','SUPPORT_DATA',\
+    tropomi_no2.latitude_corner = _read_group_nc(fname,3,['PRODUCT','SUPPORT_DATA',\
                                         'GEOLOCATIONS'],'latitude_bounds').astype('float32')
-    satellite.longitude_corner = _read_group_nc(fname,3,['PRODUCT','SUPPORT_DATA',\
+    tropomi_no2.longitude_corner = _read_group_nc(fname,3,['PRODUCT','SUPPORT_DATA',\
                                         'GEOLOCATIONS'],'longitude_bounds').astype('float32')
     # read total amf
     amf_total = _read_group_nc(fname,1,'PRODUCT','air_mass_factor_total')
     # read trop no2
     vcd = _read_group_nc(fname,1,'PRODUCT','nitrogendioxide_tropospheric_column').astype('float32')
-    satellite.vcd = vcd*6.02214*1e19*1e-15
+    scd = _read_group_nc(fname,1,'PRODUCT','nitrogendioxide_tropospheric_column').astype('float32')*\
+          _read_group_nc(fname,1,'PRODUCT','air_mass_factor_troposphere')
+    tropomi_no2.vcd = vcd*6.02214*1e19*1e-15
+    tropomi_no2.scd = scd*6.02214*1e19*1e-15
     # read quality flag
-    satellite.qa = _read_group_nc(fname,1,'PRODUCT','qa_value').astype('float32')
+    tropomi_no2.qa = _read_group_nc(fname,1,'PRODUCT','qa_value').astype('float32')
     # read pressures for SWs
     tm5_a = _read_group_nc(fname,1,'PRODUCT','tm5_constant_a')
     tm5_a = np.concatenate((tm5_a[:,0], 0), axis = None)
@@ -62,13 +66,25 @@ def tropomi_reader_no2_wrapper(fname) -> satellite:
     for z in range(0,34):
         p_mid[z,:,:] = 0.5*(tm5_a[z]+tm5_b[z]*ps[:,:]+tm5_a[z+1]+tm5_b[z+1]*ps[:,:])/100
         SWs[z,:,:] = AKs[:,:,z]*amf_total
-    satellite.pressure_mid = p_mid
-    satellite.averaging_kernels = SWs
+    tropomi_no2.pressure_mid = p_mid
+    tropomi_no2.averaging_kernels = SWs
+    # read the tropopause layer index
+    trop_layer = _read_group_nc(fname,1,'PRODUCT','tm5_tropopause_layer_index')
+    tropomi_no2.tropopause = np.zeros_like(trop_layer).astype('float32')
+    for i in range(0,np.shape(trop_layer)[0]):
+        for j in range(0,np.shape(trop_layer)[1]):
+            if trop_layer[i,j]>0:
+               tropomi_no2.tropopause[i,j] = p_mid[trop_layer[i,j],i,j]
+            else: 
+               tropomi_no2.tropopause[i,j] = np.nan
     # read the precision
-    satellite.uncertainty = _read_group_nc(fname,1,'PRODUCT',
+    tropomi_no2.uncertainty = _read_group_nc(fname,1,'PRODUCT',
                             'nitrogendioxide_tropospheric_column_precision').astype('float32')
+    # interpolation
+    if interpolation_flag == True:
+        interpolator(1,1.0,tropomi_no2,geos_model)
     # return
-    return satellite
+    return tropomi_no2
 
 def tropomi_reader(product_dir:str,num_job = 1):
     '''
@@ -82,22 +98,41 @@ def tropomi_reader(product_dir:str,num_job = 1):
     outputs = Parallel(n_jobs=num_job)(delayed(tropomi_reader_no2_wrapper)(L2_files[k]) for k in range(len(L2_files)))
 
 class readers(object):
-    def __init__(self,product_name:int,product_dir:Path,num_job = 1) -> None:
+    def __init__(self) -> None:
+        pass
+    def add_satellite_data(self,product_tag:int,product_dir:Path):
         '''
-            Initialize the reader object
+            add L2 data
             Input:
-                product_name [int]: an index specifying the type of data to read:
+                product_tag [int]: an index specifying the type of data to read:
                                    1 > TROPOMI_NO2
                                    2 > TROPOMI_HCHO
                                    3 > TROPOMI_CH4
                                    4 > TROPOMI_CO     
                 product_dir  [Path]: a path object describing the path of L2 files
         '''
-        self.product_dir = product_dir
+        self.satellite_product_dir = product_dir
+        self.satellite_product_num = product_tag
 
-        if product_name == 1:
-           tropomi_reader(self.product_dir.stem,num_job = num_job)
+    def add_ctm_data(self,product_tag:int,product_dir:Path):
+        '''
+            add CTM data
+            Input:
+                product_tag [int]: an index specifying the type of data to read:
+                                   1 > GEOS-CCM   
+                product_dir  [Path]: a path object describing the path of CTM files
+        '''
+
+        self.ctm_product_dir = product_dir
+        self.ctm_product_num = product_tag
+    
+    def read_satellite_data(self, num_job = 1):
+        if self.satellite_product_num == 1: #TROPOMI NO2
+           tropomi_reader(self.satellite_product_dir.stem, num_job = num_job)
+        
 # testing
 if __name__ == "__main__":
 
-    reader_obj = readers(1,Path('download_bucket/'),1)
+    reader_obj = readers()
+    reader_obj.add_satellite_data(1,Path('download_bucket/'))
+    reader_obj.read_satellite_data()
