d0224c5281b2f7973891e9ac8199437c5a42face
Amir Souri <ahsouri@gmail.com>
2023-02-08 10:21:33 -0500

    update config

--- caramel/config.py
+++ caramel/config.py
@@ -1,2 +1,26 @@
 import numpy as np
+from dataclasses import dataclass
+import datetime
 
+@dataclass
+class satellite:
+    vcd: np.ndarray
+    time: datetime.datetime
+    profile: np.ndarray
+    tropopause: np.ndarray
+    latitude_corner: np.ndarray
+    longitude_corner: np.ndarray
+    uncertainty: np.ndarray
+    quality_flag: np.ndarray
+    pressure_mid: np.ndarray
+    averaging_kernels: np.ndarray
+    amf_total: np.ndarray
+
+@dataclass
+class ctm_model:
+    latitude: np.ndarray
+    longitude: np.ndarray
+    gas_profile: dict
+    pressure_mid: np.ndarray
+    tempeature_mid: np.ndarray
+    delta_p: np.ndarray
--- caramel/reader.py
+++ caramel/reader.py
@@ -1,25 +1,15 @@
 import numpy as np
 import os
 from pathlib import Path
-from dataclasses import dataclass
 import datetime
 import glob
 from joblib import Parallel, delayed
 from netCDF4 import Dataset
+from config import satellite
+import warnings
 
-@dataclass
-class satellite:
-    vcd: np.ndarray
-    time: datetime.datetime
-    profile: np.ndarray
-    tropopause: np.ndarray
-    latitude_corner: np.ndarray
-    longitude_corner: np.ndarray
-    uncertainty: np.ndarray
-    quality_flag: np.ndarray
-    pressure_mid: np.ndarray
-    averaging_kernels: np.ndarray
-    amf_total: np.ndarray
+
+warnings.filterwarnings("ignore",category=RuntimeWarning)
 
 def _read_nc(filename,var):
     # reading nc files without a group
@@ -41,51 +31,54 @@ def _read_group_nc(filename,num_groups,group,var):
     nc_fid.close()
     return np.squeeze(out)
 
-def tropomi_reader_no2(product_dir:str,num_job = 1) -> satellite:
+def tropomi_reader_no2_wrapper(fname):
+    # a wrapper to run in parallel
+    # read time
+    time = np.array(_read_group_nc(fname,1,'PRODUCT','time')) +\
+           np.nanmean(np.array(_read_group_nc(fname,1,'PRODUCT','delta_time')), axis=0)/1000
+    time = np.squeeze(time)
+    satellite.time = datetime.datetime(2010, 1, 1) + datetime.timedelta(seconds=int(time))
+    # read lat/lon at corners
+    satellite.latitude_corner = np.array(_read_group_nc(fname,3,['PRODUCT','SUPPORT_DATA',\
+                                        'GEOLOCATIONS'],'latitude_bounds'))
+    satellite.longitude_corner = np.array(_read_group_nc(fname,3,['PRODUCT','SUPPORT_DATA',\
+                                        'GEOLOCATIONS'],'longitude_bounds'))
+    # read total amf
+    amf_total = _read_group_nc(fname,1,'PRODUCT','air_mass_factor_total')
+    # read trop no2
+    vcd = _read_group_nc(fname,1,'PRODUCT','nitrogendioxide_tropospheric_column')
+    satellite.vcd = vcd*6.02214*1e19*1e-15
+    # read quality flag
+    satellite.qa = _read_group_nc(fname,1,'PRODUCT','qa_value')
+    # read pressures for SWs
+    tm5_a = np.array(_read_group_nc(fname,1,'PRODUCT','tm5_constant_a'))
+    tm5_a = np.concatenate((tm5_a[:,0], 0), axis=None)
+    tm5_b = np.array(_read_group_nc(fname,1,'PRODUCT','tm5_constant_b'))
+    tm5_b = np.concatenate((tm5_b[:,0], 0), axis=None)
+    ps    = np.array(_read_group_nc(fname,3,['PRODUCT','SUPPORT_DATA','INPUT_DATA'],'surface_pressure'))
+    p_mid = np.zeros((34,np.shape(vcd)[0], np.shape(vcd)[1]))
+    SWs   = np.zeros((34,np.shape(vcd)[0], np.shape(vcd)[1]))
+    AKs   = np.array(_read_group_nc(fname,1,'PRODUCT','averaging_kernel'))
+    for z in range(0,34):
+        p_mid[z,:,:] = 0.5*(tm5_a[z]+tm5_b[z]*ps[:,:]+tm5_a[z+1]+tm5_b[z+1]*ps[:,:])/100
+        SWs[z,:,:] = AKs[:,:,z]*amf_total
+    satellite.pressure_mid = p_mid
+    satellite.averaging_kernels = SWs
+    # read the precision
+    satellite.uncertainty = np.array(_read_group_nc(fname,1,'PRODUCT','nitrogendioxide_tropospheric_column_precision'))
+    # return
+    return satellite
+
+def tropomi_reader(product_dir:str,num_job = 1) -> satellite:
     '''
-        reading tropomi no2 data
+        reading tropomi data
         Output [tropomi]: the tropomi @dataclass
     '''
-    def reader_wrapper(fname):
-        # a wrapper to run in parallel
-        # read time
-        time = np.array(_read_group_nc(fname,1,'PRODUCT','time')) +\
-               np.nanmean(np.array(_read_group_nc(fname,1,'PRODUCT','delta_time')), axis=0)/1000
-        time = np.squeeze(time)
-        satellite.time = datetime.datetime(2010, 1, 1) + datetime.timedelta(seconds=int(time))
-        # read lat/lon at corners
-        satellite.latitude_corner = np.array(_read_group_nc(fname,1,'PRODUCT','latitude'))
-        satellite.longitude_corner = np.array(_read_group_nc(fname,1,'PRODUCT','longitude'))
-        # read total amf
-        amf_total = _read_group_nc(fname,1,'PRODUCT','air_mass_factor_total')
-        # read trop no2
-        vcd = _read_group_nc(fname,1,'PRODUCT','nitrogendioxide_tropospheric_column')
-        satellite.vcd = vcd*6.02214*1e19*1e-15
-        # read quality flag
-        satellite.qa = _read_group_nc(fname,1,'PRODUCT','qa_value')
-        # read pressures for SWs
-        tm5_a = np.array(_read_group_nc(fname,1,'PRODUCT','tm5_constant_a'))
-        tm5_a = np.concatenate((tm5_a[:,0], 0), axis=None)
-        tm5_b = np.array(_read_group_nc(fname,1,'PRODUCT','tm5_constant_b'))
-        tm5_b = np.concatenate((tm5_b[:,0], 0), axis=None)
-        ps    = np.array(_read_group_nc(fname,3,['PRODUCT','SUPPORT_DATA','INPUT_DATA'],'surface_pressure'))
-        p_mid = np.zeros((34,np.shape(vcd)[0], np.shape(vcd)[1]))
-        SWs   = np.zeros((34,np.shape(vcd)[0], np.shape(vcd)[1]))
-        AKs   = np.array(_read_group_nc(fname,1,'PRODUCT','averaging_kernel'))
-        for z in range(0,34):
-            p_mid[z,:,:] = 0.5*(tm5_a[z]+tm5_b[z]*ps[:,:]+tm5_a[z+1]+tm5_b[z+1]*ps[:,:])/100
-            SWs[z,:,:] = AKs[:,:,z]*amf_total
-        satellite.pressure_mid = p_mid
-        satellite.averaging_kernels = SWs
-        # read the precision
-        satellite.uncertainty = np.array(_read_group_nc(fname,1,'PRODUCT','nitrogendioxide_tropospheric_column_precision'))
-        # return
-        return satellite
 
     # find L2 files first
     L2_files = sorted(glob.glob(product_dir + "/*.nc"))
     # read the files in parallel
-    outputs = Parallel(n_jobs=num_job)(delayed(reader_wrapper)(L2_files[k]) for k in range(len(L2_files)))
+    outputs = Parallel(n_jobs=num_job)(delayed(tropomi_reader_no2_wrapper)(L2_files[k]) for k in range(len(L2_files)))
 
 class readers(object):
     def __init__(self,product_name:int,product_dir:Path,num_job = 1) -> None:
@@ -102,7 +95,7 @@ class readers(object):
         self.product_dir = product_dir
 
         if product_name == 1:
-           self.output = tropomi_reader_no2(self.product_dir.stem,num_job = num_job)
+           self.output = tropomi_reader(self.product_dir.stem,num_job = num_job)
 # testing
 if __name__ == "__main__":
 
